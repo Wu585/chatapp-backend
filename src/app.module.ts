53ff9396c422e265e1edaee4af2250def5c6af33
wu649782027 <15151851516@163.com>
2024-01-10 18:36:08 +0800

    响应拦截器

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
 import { PrismaService } from "./prisma.service";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { TransformInterceptor } from "./interceptors/transform.interceptor";
 
 @Module({
   imports: [AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService,PrismaService],
+  providers: [
+    AppService,
+    PrismaService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor
+    }
+  ]
 })
-export class AppModule {}
+export class AppModule {
+}
