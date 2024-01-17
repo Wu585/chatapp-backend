55ce0c4889f15d7e3416abaf2ddc752f88c4ec6c
Wu <15151851516@163.com>
2024-01-17 23:15:57 +0800

    change key

--- src/replicate/replicate.service.ts
+++ src/replicate/replicate.service.ts
@@ -8,7 +8,7 @@ export class ReplicateService {
 
   constructor() {
     this.replicate = new Replicate({
-      auth: process.env.REPLICATE_API_TOKEN
+      auth: process.env.REPLICATE_API_TOKEN1
     });
   }
 }
