d7bc12900205e41a069fff156690f805ef461b9b
wu649782027 <15151851516@163.com>
2024-01-19 17:17:36 +0800

    tmp

--- src/chats/dto/create-chat.dto.ts
+++ src/chats/dto/create-chat.dto.ts
@@ -4,8 +4,8 @@ export class CreateChatDto {
   @IsNotEmpty({
     message: "标题不能为空!"
   })
-  @Length(3, 32, {
-    message: "标题长度必须在3-32个字符之间！"
+  @Length(1, 32, {
+    message: "标题长度必须在1-32个字符之间！"
   })
   title: string;
 }
